f0b0e60a757db58d3fbf27b694929d20d38f4979
Ceylon Craft <dev@ceyloncraft.com>
2026-04-15 21:00:28 +0530

    make Payment order_id nullable to allow payments without orders

--- database/migrations/2026_04_14_000008_create_payments_table.php
+++ database/migrations/2026_04_14_000008_create_payments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             
             // Foreign keys
             $table->foreignId('order_id')
+                  ->nullable()
                   ->constrained('orders')
                   ->onDelete('cascade');
             $table->foreignId('user_id')
